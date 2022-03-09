d269648230e179800ed8b73872d93f5836fef203
ElectricBeat <96882916+ElectricBeat@users.noreply.github.com>
2022-03-09 07:11:17 +0530

    LYCASOFT
    LYCASOFT TRINNING CODE FOR ARDUINO UNO STEPPER MOTOR OPERATE WITH SWITCH

---
+++ STEPPER_MOTOR.ino
@@ -0,0 +1,74 @@
+const int buttonPin_1 = 8;   
+int buttonState_1 = 0; 
+const int buttonPin_2 = 9;   
+int buttonState_2 = 0; 
+
+int a,b;
+
+void setup() 
+{
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(buttonPin_1, INPUT); 
+  pinMode(buttonPin_2, INPUT); 
+}
+
+void loop() 
+{
+  buttonState_1 = digitalRead(buttonPin_1);
+  buttonState_2 = digitalRead(buttonPin_2);
+  
+  if (buttonState_1 == HIGH) 
+  {
+    a=1;
+    b=0;
+  } 
+
+   if(a==1)
+   {
+    digitalWrite(5, LOW);
+    digitalWrite(2, HIGH);
+    delay(2);
+    digitalWrite(2, LOW);
+    digitalWrite(4, HIGH);
+    delay(2);
+    digitalWrite(4, LOW);
+    digitalWrite(3, HIGH);
+    delay(2);
+    digitalWrite(3, LOW);
+    digitalWrite(5, HIGH);
+    delay(2);
+  }
+  
+  
+  if (buttonState_2 == HIGH) 
+  {
+    a=0;
+    b=1;
+  }
+
+  if(b==1)
+  {
+    digitalWrite(2, LOW);
+    digitalWrite(5, HIGH);
+    delay(2);
+    digitalWrite(5, LOW);
+    digitalWrite(3, HIGH);
+    delay(2);
+    digitalWrite(3, LOW);
+    digitalWrite(4, HIGH);
+    delay(2);
+    digitalWrite(4, LOW);
+    digitalWrite(2, HIGH);
+    delay(2);
+  }
+  else
+  {
+    digitalWrite(2, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(2, LOW);
+    digitalWrite(5, LOW);
+  }
+}
